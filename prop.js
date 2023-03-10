9a3ea0843d7f045e001b4bb521627cf13ac0e269
Davood Keshmiri <davekeshmiri@Davoods-MacBook-Pro.local>
2023-03-10 15:17:45 -0600

    first commit

---
+++ prop.js
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div className="App">
+            <SaleItem prop1={ value1 } />
+            <SaleItem prop1={ value2 } />
+            <SaleItem prop1={ value3 } />
+        </div>
+    );
+}
+
